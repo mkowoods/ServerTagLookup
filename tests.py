c385e7c5c3a67ab903a3e8987aa442e54b8b7e5f
M Woods <mkowoods@gmail.com>
2016-05-30 20:27:24 -0700

    Clean up lines added more documentation and standardized first set of test procedures.

--- tests.py
+++ tests.py
@@ -1,8 +1,10 @@
 import server_tag_lookup as stl
 import unittest
+import sys
 
 
-# TODO: Need to add more test case
+# TODO: Need to add more test cases
+
 
 class STLTestCases(unittest.TestCase):
 
@@ -11,16 +13,15 @@ class STLTestCases(unittest.TestCase):
         resp = stl.get_resp(url)
         self.assertNotEqual(resp.url, url, 'URL Returned should differ from original url per redirects')
 
-
     def test_dell_tag_DGDHXP1(self):
-        #print 'running test for dell lookup'
+        """Test for Dell API using Server Tag 'DGDHXP1'"""
         dell = stl.DellLookUp('DGDHXP1')
-        shipping_data = dell.header_data['Shipping Date']
+        product_number = dell.header_data['product_number']
         data_len = len(dell.detail_data)
-        actual, expected = (shipping_data, data_len), ('1/21/2011', 34)
+        actual, expected = (product_number, data_len), ('PowerEdge R510', 34)
         self.assertEqual(actual, expected,
                          'Attributes Don''t Match for DGDHXP1 \
-                         Actual(product_number, detail_data_len): % s Expected: % s' \
+                         Actual(product_number, detail_data_len): %s Expected: %s'
                          % (str(actual), str(expected)))
 
     def test_ibm_tag_lookup_7944_KQ095DA(self):
@@ -32,8 +33,8 @@ class STLTestCases(unittest.TestCase):
         actual, expected = (product_number, data_len), ('7944-AC1', 18)
         self.assertEqual(actual, expected,
                          'Attributes dont match for IBM Lookup, tag: 7944|KQ095DA \
-                          Actual(product_number, detail_data_len): % s Expected: % s' \
-                         % (str(actual), str(expected)))
+                          Actual(product_number, detail_data_len): %s \
+                          Expected: %s' % (str(actual), str(expected)))
 
     def test_hp_lookup_MXQ03402B7(self):
         """Test for HP API using tag MXQ03402B7 to verify"""
@@ -49,8 +50,6 @@ class STLTestCases(unittest.TestCase):
                          Expected: %s' % (str(actual), str(expected)))
 
 
-import sys
-
 def execute_test_suite():
     suite = unittest.TestLoader().loadTestsFromTestCase(STLTestCases)
-    unittest.TextTestRunner(stream = sys.stdout, verbosity=2).run(suite)
+    unittest.TextTestRunner(stream=sys.stdout, verbosity=2).run(suite)
