2d8b6578f638c7f887bcca3039d553739cd46ac8
Derek Castile <DCastile@Curvature.com>
2016-06-07 19:57:15 -0700

    Update excel_tools.py

--- excel_tools.py
+++ excel_tools.py
@@ -1,1 +1,18 @@
 # TODO add excel integration here
+
+
+class QuoteWorkbook(object):
+    
+    def save(self, url):
+        pass
+
+    def load(self, url):
+        pass
+
+    def fetch_quote_line(self, line_number):
+        pass
+
+    def write_equipment_schedule(self, line_number, system, component_list):
+        # write system
+        # write quote line
+        # write component lines
