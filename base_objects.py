88e56175171c9007b271b8e4cc15be965a6127cf
Derek Castile <DCastile@Curvature.com>
2016-06-07 20:05:10 -0700

    Create base_objects.py

---
+++ base_objects.py
@@ -0,0 +1,22 @@
+
+
+class System(object):
+    def __init__(self, system_tag):
+        pass
+
+   def load_json(self, json):
+        pass
+
+    def get_json(self)
+        pass
+
+ 
+
+
+
+class Component(object):
+     def load_json(self, json):
+        pass
+
+    def get_json(self)
+        pass
